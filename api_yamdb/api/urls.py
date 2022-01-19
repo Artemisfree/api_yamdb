4c852e78f4fa489e667beeced8d15ed63b6427a6
Olesya <lesenysh@yandex.ru>
2022-01-19 12:58:03 +0300

    Категории, жанры, произведения

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'genres', GenreViewSet)
+router.register(r'titles', TitleViewSet)
+
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+]
